9e9fefb181c1db291675e103ebd64ec38d0679b4
bonheurisme <100709185+callmejippo@users.noreply.github.com>
2023-12-07 14:40:25 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+const port = 4000;
+const expressHbs = require("express-handlebars");
+
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
+// Thiet lap thu muc Static
+app.use(express.static(__dirname + "/html"));
+
+// Cau hinh Template Engine
+app.engine(
+  "hbs",
+  expressHbs.engine({
+    layoutsDir: __dirname + "/views/layouts",
+    defaultLayout: "layout",
+    extname: "hbs",
+    runtimeOptions: {
+      allowProtoPropertiesByDefault: true,
+    },
+  })
+);
+app.set("view engine", "hbs");
+
+//thiet lap su dung cookies
+app.use(cookieParser("COOKIE_SECRET"));
+
+//thiet lap su dung session va luu tru session tren Reids
+app.use(
+  session({
+    secret: "SESSION_SECRET",
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+      secure: false, //if true only transmit cookie over https
+      httpOnly: true, //prevet client side JS from reading the cookie
+      maxAge: 20 * 60 * 1000, // 20 phut
+    },
+  })
+);
+
+// Cau hinh cho phep doc du lieu gui len bang phuong thuc POST
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Chuyen huong route xu ly
+app.use("/", require("./routes/authRouter"));
+
+// Start web server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
